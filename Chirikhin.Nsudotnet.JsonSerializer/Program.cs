9b15e5599433458313cd33b51ef34cf1affd2689
WildWind03 <usergreat03@gmail.com>
2017-05-07 16:54:39 +0700

    Ready to check

---
+++ Chirikhin.Nsudotnet.JsonSerializer/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using static System.String;
+
+namespace JSONSerializer
+{
+    [Serializable]
+    public class TestClass
+    {
+        public TestNestedClass TestNestedClass = new TestNestedClass(3, 4);
+        public bool TestBoolean = true;
+        public int TestIntegerValue = 5;
+        public string TestStringValue = "TestString\"";
+        public double Hah = double.NaN; //Accroding to JSON standart NaN values shoudn't be serialized
+        public decimal TestDecimalValues = new decimal(0.5);
+        public DateTime TestDateTime = DateTime.Now;
+        public TestNestedClass TestNestedClassNull = null;
+
+        [NonSerialized] public string Ignore = null; //Non serialized values won't be serialized
+
+        public TestNestedClass[] ArrayMember = {new TestNestedClass(1, 2), new TestNestedClass(3, 4)};
+        public List<string> StringList = new List<string>();
+        public Dictionary<int, int> Dictionary = new Dictionary<int, int>();
+        public List<string> List = new List<string>();
+
+        public TestClass()
+        {
+            Dictionary.Add(45, 21);
+            Dictionary.Add(34, 56);
+
+            StringList.Add("Alexander");
+            StringList.Add("Chirikhin");
+        }
+    }
+
+    public class TestNestedClass
+    {
+        public int NestedValue1;
+        public int NestedValue2;
+
+        public TestNestedClass(int value1, int value2)
+        {
+            NestedValue1 = value1;
+            NestedValue2 = value2;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(JsonSerializer.ToJson(new TestClass()));
+            Console.ReadKey();
+        }
+    }
+
+    public static class JsonSerializer
+    {
+
+        private static bool IsNanOrInfinity(this object obj)
+        {
+            if (obj is double)
+            {
+                return double.IsNaN((double) obj) || double.IsInfinity((double) obj);
+            }
+
+            if (obj is float)
+            {
+                return float.IsNaN((float) obj) || float.IsInfinity((float) obj);
+            }
+
+            return false;
+        }
+
+        public static string ToJson(object objectToSerialize)
+        {
+            if (null == objectToSerialize)
+            {
+                return "null";
+            }
+
+            if (!objectToSerialize.GetType().IsSerializable)
+            {
+                return null;
+            }
+
+            if (objectToSerialize.GetType().IsPrimitive || objectToSerialize is decimal)
+            {
+                return IsNanOrInfinity(objectToSerialize) ? null : 
+                    Format(CultureInfo.InvariantCulture, "{0}", objectToSerialize.ToString().ToLower());
+            }
+
+            if (objectToSerialize is string || 
+                objectToSerialize is DateTime || 
+                objectToSerialize is TimeSpan || 
+                objectToSerialize is DateTimeOffset)
+            {
+                var serializableString = objectToSerialize.ToString().Replace("\"", "\\\"");
+                return Concat("\"", serializableString, "\"");
+            }
+
+            if (objectToSerialize is IDictionary)
+            {
+                var jsonDictionaryBuilder = new StringBuilder();
+                jsonDictionaryBuilder.Append("{");
+
+                var iDictionary = (IDictionary) objectToSerialize;
+
+                foreach (var i in iDictionary.Keys)
+                {
+                    var value = iDictionary[i];
+                    jsonDictionaryBuilder.Append(Concat("\"", i, "\":", ToJson(value), ","));
+                }
+
+                var jsonEnumerableString = jsonDictionaryBuilder.ToString();
+                if (jsonEnumerableString[jsonEnumerableString.Length - 1] == ',')
+                {
+                    jsonEnumerableString = jsonEnumerableString.Substring(0, jsonEnumerableString.Length - 1);
+                }
+
+                return jsonEnumerableString + "}";
+            }
+
+            if (objectToSerialize is IEnumerable)
+            {
+                var jsonEnumerableBuilder = new StringBuilder();
+                jsonEnumerableBuilder.Append("[");
+
+                foreach (var i in (IEnumerable) objectToSerialize)
+                {
+                    if (i.GetType().IsSerializable)
+                    {
+                        jsonEnumerableBuilder.Append(ToJson(i) + ",");
+                    }
+                }
+
+                var jsonEnumerableString = jsonEnumerableBuilder.ToString();
+                if (jsonEnumerableString[jsonEnumerableString.Length - 1] == ',')
+                {
+                    jsonEnumerableString = jsonEnumerableString.Substring(0, jsonEnumerableString.Length - 1);
+                }
+
+                return jsonEnumerableString + "]";
+            }
+
+            var jsonBuilder = new StringBuilder();
+
+            jsonBuilder.Append("{");
+
+            var fieldValues = objectToSerialize.GetType().GetFields(BindingFlags.Public |
+                                                                    BindingFlags.NonPublic
+                                                                    | BindingFlags.Instance);
+            foreach (var fieldInfo in fieldValues)
+            {
+                if (fieldInfo.IsNotSerialized)
+                {
+                    continue;
+                }
+
+                var serializedField = ToJson(fieldInfo.GetValue(objectToSerialize));
+
+                if (null == serializedField)
+                {
+                    continue;
+                }
+
+                jsonBuilder.Append(Concat("\"", fieldInfo.Name, "\":", serializedField, ","));
+            }
+
+            var serializedObjects = jsonBuilder.ToString();
+
+            if (serializedObjects[serializedObjects.Length - 1] == ',')
+            {
+                serializedObjects = serializedObjects.Substring(0, serializedObjects.Length - 1);
+            }
+
+            return serializedObjects + "}";
+        }
+    }
+}
---
+++ Chirikhin.Nsudotnet.NumberGuesser/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        private const int MaxRandomNumber = 100;
+        private const int MaxCoundOfFailedAttempts = 1000;
+        private const int OffensiveCommentsFrequency = 4;
+        private const int CountOfComments = 4;
+        private const string AuthoName = "Alexander Chirikhin";
+        private const string QuitString = "q";
+
+        static void Main()
+        {
+            var startDateTime = DateTime.Now;
+            Console.WriteLine("Please, type your name");
+            var userName = Console.ReadLine();
+
+            var offensiveComments = new string[CountOfComments];
+
+            if (userName != AuthoName)
+            {
+                Console.WriteLine("What's a crappy name! Just type a number and pray that I'll come up with the same!");
+
+                offensiveComments[0] = "It's Roman Posokhin, isn't it? Even if it isn't, your smell and your creepy face make me suppose that your are his sibling! What are you talking?! You are {0}?! Then I'm sorry! You just a piece of crap, nothing more!";
+                offensiveComments[1] = "Come on, {0}! Don't be like Roman Posokhin!";
+                offensiveComments[2] = "Your 'intellegence' is your major flaw! {0}, you are from Omsk, aren't you?";
+                offensiveComments[3] = "{0}, are really so foolish?! It's incredible that you exist despite the evolution!";
+
+            } else
+            {
+                Console.WriteLine("Welcome, brother! Glad to see you again! Please, type a number to start the game!");
+                offensiveComments[0] = "Hold on, {0}! Together we'll win!";
+                offensiveComments[1] = "{0}, just believe and you'll make it!";
+                offensiveComments[2] = "Don't worry, {0}! I feel that the victory is so near!";
+                offensiveComments[3] = "{0}, you are the smartest man I've ever met! So cool!";
+            }
+
+            var failedAttempts = new int[MaxCoundOfFailedAttempts];
+            var countOfFailedAttempts = 0;
+            var random = new Random();
+
+            int randomNumber = random.Next() % MaxRandomNumber;
+
+            for (var k = 0; ; k++)
+            {
+                var typedNumberString = Console.ReadLine();
+
+                if (typedNumberString == QuitString)
+                {
+                    Console.WriteLine("Buzz like never before!");
+                    break;
+                }
+
+                try
+                {
+                    var typedNumberInteger = int.Parse(typedNumberString);
+
+                    if (typedNumberInteger == randomNumber)
+                    {
+                        Console.WriteLine("Congratulations!");
+                        Console.WriteLine("Attempts Count: {0}", countOfFailedAttempts + 1);
+                        Console.Write("History: ");
+
+                        for (var i = 0; i < countOfFailedAttempts; ++i)
+                        {
+                            var lessOrGreaterLabel = failedAttempts[i] > randomNumber ? ">" : "<";
+                            Console.Write(" {0}({1})", failedAttempts[i], lessOrGreaterLabel);
+                        }
+
+                        Console.Write(" {0}({1})", randomNumber, "=");
+
+                        Console.WriteLine();
+                        Console.WriteLine("Count of passed seconds: {0}", (DateTime.Now - startDateTime).Seconds);
+                        break;
+                    }
+                    if (countOfFailedAttempts >= MaxCoundOfFailedAttempts - 1)
+                    {
+                        Console.WriteLine("Too many attempts. You are just a looser!");
+                        break;
+                    }
+
+                    failedAttempts[countOfFailedAttempts++] = typedNumberInteger;
+
+                    if (typedNumberInteger < randomNumber)
+                    {
+                        Console.WriteLine("Your number is less");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your number is greater");
+                    }
+
+                    if (0 == countOfFailedAttempts % (OffensiveCommentsFrequency - 1))
+                    {
+                        Console.WriteLine(offensiveComments[random.Next() % CountOfComments], userName);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("What kind of boolshit you have typed!? It's even not a number!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Put such numbers into your tremendous butthole!");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
